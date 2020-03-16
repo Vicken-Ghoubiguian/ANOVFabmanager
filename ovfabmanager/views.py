070981d6779e1b1e3f205b3073a8d0b587e2ebe5
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-03-16 09:43:00 +0100

    Implémentation d'une page pour le calendrier

--- ovfabmanager/views.py
+++ ovfabmanager/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'ovfabmanager/index.html', {})
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'index'})
+
+def calendrier(request):
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'calendrier'})
