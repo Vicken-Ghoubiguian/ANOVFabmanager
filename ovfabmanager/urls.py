301fbff5c5e70b95b248dfeff4c6a74bd95797c1
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-03-17 10:35:26 +0100

    Définition des autres URL pour le client

--- ovfabmanager/urls.py
+++ ovfabmanager/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.accueil, name='accueil'),
     path('calendrier', views.calendrier, name='calendrier'),
+    path('reserver_une_machine', views.reserver_une_machine, name='reserver_une_machine'),
+    path('reserver_un_espace', views.reserver_un_espace, name='reserver_un_espace'),
+    path('inscriptions_aux_formations', views.inscriptions_aux_formations, name='inscriptions_aux_formations'),
+    path('inscriptions_aux_evenements', views.inscriptions_aux_evenements, name='inscriptions_aux_evenements'),
+    path('galerie_de_projets', views.galerie_de_projets, name='galerie_de_projets'),
+    path('tarifs_et_abonnements', views.tarifs_et_abonnements, name='tarifs_et_abonnements'),
 ]
--- ovfabmanager/views.py
+++ ovfabmanager/views.py
@@ -7,3 +7,21 @@ def accueil(request):
 
 def calendrier(request):
     return render(request, 'ovfabmanager/index.html', {'template_demandee': 'calendrier'})
+
+def reserver_une_machine(request):
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'reserver_une_machine'})
+
+def reserver_un_espace(request):
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'reserver_un_espace'})
+
+def inscriptions_aux_formations(request):
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'inscriptions_aux_formations'})
+
+def inscriptions_aux_evenements(request):
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'inscriptions_aux_evenements'})
+
+def galerie_de_projets(request):
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'galerie_de_projets'})
+
+def tarifs_et_abonnements(request):
+    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'tarifs_et_abonnements'})
