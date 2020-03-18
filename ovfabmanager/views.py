80b3f31aee192362aa51aabfbc325b8df4366387
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-03-18 09:04:35 +0100

    Déployement d'une variable pour l'identification de l'erreur

--- ovfabmanager/views.py
+++ ovfabmanager/views.py
@@ -4,65 +4,66 @@ from django.shortcuts import render
 
 # For common
 def accueil(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'accueil'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'accueil'})
 
 def calendrier(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'calendrier'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'calendrier'})
 
 def reserver_une_machine(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'reserver_une_machine'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'reserver_une_machine'})
 
 def reserver_un_espace(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'reserver_un_espace'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'reserver_un_espace'})
 
 def inscriptions_aux_formations(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'inscriptions_aux_formations'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'inscriptions_aux_formations'})
 
 def inscriptions_aux_evenements(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'inscriptions_aux_evenements'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'inscriptions_aux_evenements'})
 
 def galerie_de_projets(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'galerie_de_projets'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'galerie_de_projets'})
 
 def common_tarifs_et_abonnements(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'common_tarifs_et_abonnements'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'common_tarifs_et_abonnements'})
 
 # For admin
 def gestion_des_paniers(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gestion_des_paniers'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gestion_des_paniers'})
 
 def gestion_des_stocks(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gestion_des_stocks'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gestion_des_stocks'})
 
 def gerer_le_calendrier(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gerer_le_calendrier'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gerer_le_calendrier'})
 
 def gerer_les_utilisateurs(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gerer_les_utilisateurs'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gerer_les_utilisateurs'})
 
 def gerer_les_machines(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gerer_les_machines'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gerer_les_machines'})
 
 def gerer_les_factures(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gerer_les_factures'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gerer_les_factures'})
 
 def admin_tarifs_et_abonnements(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'admin_tarifs_et_abonnements'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'admin_tarifs_et_abonnements'})
 
 def gerer_les_espaces(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gerer_les_espaces'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gerer_les_espaces'})
 
 def suivi_des_formations(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'suivi_des_formations'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'suivi_des_formations'})
 
 def gerer_les_evenements(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'gerer_les_evenements'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'gerer_les_evenements'})
 
 def statistiques(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'statistiques'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'statistiques'})
 
 def personnalisation(request):
-    return render(request, 'ovfabmanager/index.html', {'template_demandee': 'personnalisation'})
+    return render(request, 'ovfabmanager/index.html', {'afficher_erreur': False, 'type_erreur': None, 'template_demandee': 'personnalisation'})
 
+#For errors
 def error_404(request, exception):
-    return render(request, 'ovfabmanager/errors/404.html', {'template_demandee': 'personnalisation'})
+    return render(request, 'ovfabmanager/errors/404.html', {'afficher_erreur': True, 'type_erreur': '404', 'template_demandee': None})
