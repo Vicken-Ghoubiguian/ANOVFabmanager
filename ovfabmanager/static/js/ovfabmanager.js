45129951b8eff33f40b4753f4a0c95113d1bf09e
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-03-20 15:52:21 +0100

    Prise en charge par le JS pour les boutons de la barre de connection

--- ovfabmanager/static/js/ovfabmanager.js
+++ ovfabmanager/static/js/ovfabmanager.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
   $( document ).tooltip();
+
+  $("#premier").click(function(){
+        console.log("11111.....");
+  });
+
+  $("#second").click(function(){
+        console.log("22222.....");
+  });
+
+  $("#troisieme").click(function(){
+	console.log("33333.....");
+  });
 });
