bf19e753a5af29141ffaedf782001468e757ab15
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-03-24 18:26:58 +0100

    Changer le mois et l'année de naissance

--- ovfabmanager/static/js/ovfabmanager.js
+++ ovfabmanager/static/js/ovfabmanager.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $( document ).tooltip();
 
-  $("#naissance").datepicker({ minDate: -36500, maxDate: -1825 });
+  $("#naissance").datepicker({ minDate: -36500, maxDate: -1825, changeMonth: true, changeYear: true });
 
   $("#premier").click(function(){
         console.log("11111.....");
